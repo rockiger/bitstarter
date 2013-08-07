22714160ae681ee0e006ece8ae2530d777b074fa
Ubuntu <ubuntu@ip-172-31-23-155.us-west-2.compute.internal>
2013-08-07 22:01:13 +0000

    fjadskf

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var file = fs.readFileSync("index.html");
+var buffer = new Buffer(file);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(buffer.toStr());
 });
 
 var port = process.env.PORT || 5000;
